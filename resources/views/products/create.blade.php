5b4c1f69700528fbcb757ece0982ac1f3f4f959e
Rafe Khatebi <rkhatibi2003@gmail.com>
2025-12-23 11:14:20 +0430

    added new labels and inputs for setting page

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -28,7 +28,7 @@
                                 <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                             </div>
                         @endif
-                        
+
                         @if(session('error'))
                             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                 <i class="fa fa-exclamation-circle me-2"></i>{{ session('error') }}
