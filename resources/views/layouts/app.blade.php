b3d7888195c370c8f9b89a328bda4ad9e657e050
Rafe Khatebi <rkhatibi2003@gmail.com>
2025-12-24 12:43:07 +0430

    added fonts styles js files to all system work correctly in offline mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,10 @@
     <meta charset="utf-8">
     <title>@yield('title', 'Inventory System')</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
     <!-- CSS Files -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-
     @stack('styles')
 </head>
 
@@ -36,8 +34,13 @@
     </div>
 
     <!-- JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    {{--
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
+
+    {{--
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="{{ asset('assets/dist/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/dist/js/bootstrap.bundle.min.js') }}"></script>
     <script>
         // Sidebar toggle
         document.querySelector('.sidebar-toggler').addEventListener('click', function () {
--- resources/views/stocks/in.blade.php
+++ resources/views/stocks/in.blade.php
@@ -3,21 +3,8 @@
 @section('title', 'Stock In')
 
 @push('styles')
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<style>
-.select2-container--default .select2-selection--single {
-    height: 38px;
-    border: 1px solid #ced4da;
-    border-radius: 0.375rem;
-}
-.select2-container--default .select2-selection--single .select2-selection__rendered {
-    line-height: 36px;
-    padding-left: 12px;
-}
-.select2-container--default .select2-selection--single .select2-selection__arrow {
-    height: 36px;
-}
-</style>
+<link rel="stylesheet" href="{{ asset('assets/select2/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/select2/css/select2custom.css') }}">
 @endpush
 
 @section('content')
@@ -88,7 +75,7 @@
     </div>
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="{{ asset('assets/select2/js/select2.min.js') }}"></script>
 <script>
 $(document).ready(function() {
     $('.select2').select2({
